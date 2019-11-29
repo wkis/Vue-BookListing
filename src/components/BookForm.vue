a0d85785937ca957a06e6dc5084614db3c4f44dc
Wil Kisters <wkisters@home.nl>
2019-11-29 14:49:31 +0100

    Completed module

---
+++ src/components/BookForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form class="" action="#" method="post">
+      <input type="text" name="title" value="" placeholder="Book Title">
+      <input type="text" name="author" value="" placeholder="Book Author">
+      <button type="submit" name="button">Add Book</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'BookForm',
+  props: ['books'],
+  data() {
+  return {
+    bookTitle: '',
+    bookAuthor: ''
+  }
+  };
+</script>
+
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
