53886d8e1ab215786520c67afb0c74785b0cd9c7
Wil Kisters <wkisters@home.nl>
2019-11-29 16:12:02 +0100

    Completed module

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -20,7 +20,7 @@ export default {
   },
   methods:{
     bookSubmit(bookTitle, bookAuthor){
-      this.$emit('addbook', bookTitle, bookAuthor)
+      this.$emit('addBook', bookTitle, bookAuthor)
 }
   }
   };
