9001a1c9e2c1e9f4fbe88da391da22a26d7d26e1
Wil Kisters <wkisters@home.nl>
2019-11-29 13:13:27 +0100

    Completed module

---
+++ src/components/BookItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books">{{ book.title }}:{{book.author}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookItem',
+  data() {
+    return {
+      props: [
+        {'book''},
+      ]
+    };
+  },
+};
+</script>
+
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
